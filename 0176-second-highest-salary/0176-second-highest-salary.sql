f692aac84c587155598601ddcb62415548887baf
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-05-14 18:49:11 +0530

    Time: 250 ms (95.32%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-with old_cte as (select distinct salary from employee ), new_cte as (select max(salary) as secondHighestSalary from employee where salary <(select max(salary)from employee))select * from new_cte ;
+-- with old_cte as (select distinct salary from employee ), new_cte as (select max(salary) as secondHighestSalary from employee where salary <(select max(salary)from employee))select * from new_cte ;
+select distinct (select distinct  salary as SecondHighestSalary from(select salary , dense_rank() over(order by salary desc) as rnk from employee )as e where e.rnk=2) as secondHighestSalary ;
