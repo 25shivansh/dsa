9c938ca6084c3b7ea6efb4eac10a11a250a41136
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-05-21 14:42:41 +0530

    Time: 475 ms (29.18%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,7 +2,9 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-      select distinct salary from (select salary , dense_rank() over(order by salary desc) as rnk from Employee) as e where e.rnk=n 
+      -- select distinct salary from (select salary , dense_rank() over(order by salary desc) as rnk from Employee) as e where e.rnk=n
+      with cte as (select distinct salary from(select salary , dense_rank()over(order by salary desc)as rnk from employee) as e where e.rnk = n)
+      select * from cte
 
   );
 END
