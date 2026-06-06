99de2457900b1cdc1fe5b8fcff21519a912f7e40
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-06-06 14:46:35 +0530

    Time: 2 ms (88.03%), Space: 48 MB (22.54%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,36 +1,14 @@
 class Solution {
-    
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        boolean [] dp = new boolean[n+1];
-        for(int i = 0 ;i< n;i++){
-            dp[i]=false ;
-        }
-        dp[0]=true ;
-        for(int i = 1 ;i< n;i++){
-            for(int j=i-1 ; j>=0 ; j--){
-                if(dp[j]==true && j+nums[j]>=i){
-                    dp[i]=true ;
-                    break ;
-                }
-
+        int n = nums.length ;
+        int maxReach = 0;
+        for(int i = 0 ;i<n;i++){
+            if(i>maxReach){
+                return false ;
+                
             }
-
+            maxReach = Math.max(maxReach,i+nums[i]);
         }
-        return dp[n-1];
-        
-        
+        return true ;
     }
-    // private boolean solve(int []nums,int idx,int n){
-    //     if(idx == n-1)return true ;
-    //     if(idx>=n)return true ;
-    //     if(dp[idx]!=null)return dp[idx];
-    //     for(int i =1;i<=nums[idx];i++){
-    //         if(solve(nums,idx+i,n)==true){
-    //             dp[idx]=true ;
-    //             return dp[idx] ;
-    //         }
-    //     }
-    //     return dp[idx]=false  ;
-    // }
 }
