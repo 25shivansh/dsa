9f5b4606794c747c787e8b4ad84a07bf818c6c5d
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-06-06 14:32:24 +0530

    Time: 37 ms (13%), Space: 47.9 MB (41.2%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,36 @@
 class Solution {
-    Boolean[]dp;
+    
     public boolean canJump(int[] nums) {
-        dp=new Boolean[nums.length+1];
-        return solve(nums,0,nums.length);
-    }
-    private boolean solve(int []nums,int idx,int n){
-        if(idx == n-1)return true ;
-        if(idx>=n)return true ;
-        if(dp[idx]!=null)return dp[idx];
-        for(int i =1;i<=nums[idx];i++){
-            if(solve(nums,idx+i,n)==true){
-                dp[idx]=true ;
-                return dp[idx] ;
-            }
+        int n = nums.length;
+        boolean [] dp = new boolean[n+1];
+        for(int i = 0 ;i< n;i++){
+            dp[i]=false ;
         }
-        return dp[idx]=false  ;
+        dp[0]=true ;
+        for(int i = 1 ;i< n;i++){
+            for(int j=i-1 ; j>=0 ; j--){
+                if(dp[j]==true && j+nums[j]>=i){
+                    dp[i]=true ;
+                    break ;
+                }
+
+            }
+
+        }
+        return dp[n-1];
+        
+        
     }
+    // private boolean solve(int []nums,int idx,int n){
+    //     if(idx == n-1)return true ;
+    //     if(idx>=n)return true ;
+    //     if(dp[idx]!=null)return dp[idx];
+    //     for(int i =1;i<=nums[idx];i++){
+    //         if(solve(nums,idx+i,n)==true){
+    //             dp[idx]=true ;
+    //             return dp[idx] ;
+    //         }
+    //     }
+    //     return dp[idx]=false  ;
+    // }
 }
