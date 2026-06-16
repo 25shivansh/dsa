a14cf7f60da6f17ee9ea64acda0c46604aba2b4e
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-06-17 00:17:58 +0530

    Time: 213 ms (18.99%), Space: 48.3 MB (7.73%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,32 +1,34 @@
 class Solution {
-    Integer[][]dp;
     public String longestPalindrome(String s) {
-        int n = s.length();
-        dp=new Integer[n+1][n+1];
+        int n =s.length();
+        boolean[][]dp=new boolean[n][n];
         int maxLen =0;
-        int start = 0;
+        int idx =0;
+        // substring of length 1 is always pallindrom , for eg dp[0][0] or dp[1][1]
         for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-                if(solve(s,i,j)){
-                    int len =j-i+1;
-                    if(len>maxLen){
-                        maxLen = Math.max(len,maxLen);
-                        start =i;
+            dp[i][i]=true ;
+            maxLen =1;
+        }
+        // start from len = 2
+        for(int len =2;len<=n;len++){
+            for(int i =0;i<=n-len;i++){
+                int j =i+len-1;
+                if(s.charAt(i)==s.charAt(j)&&len==2){
+                    dp[i][j]=true;
+                    maxLen = 2;
+                    idx=i;
+                }else if(s.charAt(i)==s.charAt(j)&&dp[i+1][j-1]==true){
+                    dp[i][j]=true ;
+                    if(j-i+1>maxLen){
+                        maxLen = j-i+1;
+                        idx=i;
                     }
+                }else {
+                    dp[i][j]=false ;
                 }
             }
         }
-        return s.substring(start,maxLen+start);
+        return s.substring(idx,idx+maxLen);
     }
-    private boolean solve(String s ,int i,int j){
-        if(i>=j)return true ;
-        if(dp[i][j]!=null)return dp[i][j]==1;
-        if(s.charAt(i)==s.charAt(j)){
-            boolean res = solve(s,i+1,j-1);
-            dp[i][j]=res?1:0;
-            return res ;
-        }
-        dp[i][j]=0;
-        return false ;
-    }
+
 }
