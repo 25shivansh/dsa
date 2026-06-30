e82ac116e943c48166d93f0bd6fdeab42d202b64
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-07-01 02:56:56 +0530

    Time: 6 ms (85.89%), Space: 45.9 MB (92.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,19 +4,42 @@ class Solution {
         // dp=new Integer[nums.length+1][nums.length+1];
         // return solve(nums,0,-1);
         //tabulation
-        int n = nums.length ;
-        int []dp = new int [n+1];
-        int maxLis=0;
-        Arrays.fill(dp,1);
-        for(int i =0;i<n;i++){
-            for(int j =0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
+        // int n = nums.length ;
+        // int []dp = new int [n+1];
+        // int maxLis=0;
+        // Arrays.fill(dp,1);
+        // for(int i =0;i<n;i++){
+        //     for(int j =0;j<i;j++){
+        //         if(nums[j]<nums[i]){
+        //             dp[i]=Math.max(dp[i],dp[j]+1);
+        //         }
+        //     }
+        //     maxLis=Math.max(maxLis,dp[i]);
+        // }
+        // return maxLis;
+
+        // o(nlogn)approach binary search wali 
+        List<Integer>ans = new ArrayList<>();
+        for(int num:nums){
+            int left =0;
+            int right =ans.size();
+            while(left<right){
+                int mid =left+(right-left)/2;
+                if(ans.get(mid)<num){
+                    left =mid +1;
+
+                }else{
+                    right =mid;
                 }
             }
-            maxLis=Math.max(maxLis,dp[i]);
+            if(left == ans.size()){
+                ans.add(num);
+            }else{
+                ans.set(left,num);
+            }
         }
-        return maxLis;
+        return ans.size();
+
         
     }
     // private int solve(int []nums,int i,int p){
