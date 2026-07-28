6d655a38d49d334a78497cc4f8b624df838f5f86
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-07-29 01:37:29 +0530

    Time: 1539 ms (31.19%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -10,4 +10,6 @@
 -- GROUP BY v.customer_id;
 
 -- left join 
-select v.customer_id, count(*) as count_no_trans from Visits as v left join transactions as t on v.visit_id=t.visit_id where t.visit_id is null group by(v.customer_id)
+-- select v.customer_id, count(*) as count_no_trans from Visits as v left join transactions as t on v.-- visit_id=t.visit_id where t.visit_id is null group by(v.customer_id)
+
+select v.Customer_id,count(*) as count_no_trans from Visits as v where not exists(select 1 from transactions as t where t.visit_id =v.visit_id)group by v.customer_id
