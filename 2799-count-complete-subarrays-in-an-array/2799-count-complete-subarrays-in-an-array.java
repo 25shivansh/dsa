3449569c88480990a88b70e6987e7a11dc1ff6ba
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-07-29 12:55:11 +0530

    Time: 7 ms (93.95%), Space: 47.1 MB (52.09%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
@@ -1,22 +1,50 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
+        // brute force approach 
+        // HashSet<Integer>set = new HashSet<>();
+        // for(int num : nums){
+        //     set.add(num);
+        // }
+        // int unique_element = set.size();
+        // int n = nums.length ;
+        // int count =0;
+        
+        // for(int i =0;i<n;i++){
+        //     HashSet<Integer>set2=new HashSet<>();
+        //     for(int j = i;j<n;j++){
+        //         set2.add(nums[j]);
+        //         if(set2.size()==unique_element){
+        //             count ++;
+        //         }
+        //     }
+        // }
+        // return count ;
+
+        // optimal approach 
+        int n = nums.length ;
+        int left =0;
+        int count =0;
         HashSet<Integer>set = new HashSet<>();
-        for(int num : nums){
+        for(int num:nums){
             set.add(num);
         }
         int unique_element = set.size();
-        int n = nums.length ;
-        int count =0;
-        
-        for(int i =0;i<n;i++){
-            HashSet<Integer>set2=new HashSet<>();
-            for(int j = i;j<n;j++){
-                set2.add(nums[j]);
-                if(set2.size()==unique_element){
-                    count ++;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int right =0;right<n;right++){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            while(map.size()==unique_element){
+                count +=n-right;
+                map.put(nums[left],map.get(nums[left]) - 1);
+                if(map.get(nums[left])==0) {
+                    map.remove(nums[left]);
                 }
+
+                left++;
             }
         }
         return count ;
+
+        
+
     }
 }
