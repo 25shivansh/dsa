632c3ad80dd1dfd180d150199c0647b090210922
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-07-31 01:48:42 +0530

    Time: 1 ms (99.95%), Space: 77.1 MB (66.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -12,11 +12,11 @@ class Solution {
         // return maxSum;
 
         //KADANE ALGO
-        int currSum=nums[0];
+        int currSum = nums[0];
         int maxSum =nums[0];
-        for(int i=1;i<n;i++){
+        for(int i =1;i<n;i++){
             currSum=Math.max(nums[i],currSum+nums[i]);
-            maxSum=Math.max(maxSum,currSum);
+            maxSum=Math.max(currSum,maxSum);
         }
         return maxSum ;
 
