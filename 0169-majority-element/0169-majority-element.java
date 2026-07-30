fc0f9df892925e6b7773e6fa1d785771c7b03c00
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-07-31 01:41:13 +0530

    Time: 1 ms (99.89%), Space: 55.6 MB (46.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //boyre moore algo ---->
-        int candidate =-1;
+        // boyer moore algorithm 
+        int n =nums.length ;
         int count =0;
+        int candidate =0;
         for(int num:nums){
-            if(count==0){
+            if(count ==0){
                 candidate=num;
             }
             if(num==candidate){
-                count++;
-            }else{
+                count ++;
+            }else {
                 count --;
-
             }
         }
-        return candidate;
+        return candidate ;
     }
-
 }
