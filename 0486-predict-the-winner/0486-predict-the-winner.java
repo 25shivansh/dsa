e03e40ec94077864c511a4f2aa7f310ec073bd92
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-08-02 01:30:21 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.02%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,5 +1,7 @@
 class Solution {
+    Integer[][]dp;
     public boolean predictTheWinner(int[] nums) {
+        dp=new Integer[nums.length+1][nums.length+1];
         return solve(nums,0,nums.length-1)>=0;
 
     }
@@ -7,8 +9,9 @@ class Solution {
         if(i==j){
             return nums[i];
         }
+        if(dp[i][j]!=null)return dp[i][j];
         int pickRight=nums[i]-solve(nums,i+1,j);
         int pickLeft=nums[j]-solve(nums,i,j-1);
-        return Math.max(pickRight,pickLeft);
+        return dp[i][j]=Math.max(pickRight,pickLeft);
     }
 }
