b4c2ec9568d376211a997600165141c1afe61ebe
Shivansh Singh <160400936+25shivansh@users.noreply.github.com>
2026-09-11 15:20:09 +0530

    Time: 29 ms (17.16%), Space: 58.4 MB (5.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,28 +1,23 @@
 class Solution {
-    // Integer[][]dp;
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int [][]dp=new int[n+1][m+1];
-        for(int i =1;i<=n;i++){
-            for(int j =1;j<=m;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+    Integer[][]dp;
+    private int solve(String text1,String text2,int i,int j){
+        int m=text1.length();
+        int n=text2.length();
+        // base case -->out of bound 
+        if(i<0 ||i>=m||j<0||j>=n){
+            return 0;
         }
-        return dp[n][m];
-    
-    // private int solve(String s1,String s2,int i,int j){
-    //     if(i>=s1.length()||j>=s2.length()){
-    //         return 0;//no more options remaining to iterate 
-    //     }
-    //     if(dp[i][j]!=null)return dp[i][j];
-    //     if(s1.charAt(i)==s2.charAt(j)){
-    //         return 1+solve(s1,s2,i+1,j+1);//character matched , so remove the char and add 1
-    //     }
-    //     return dp[i][j]= Math.max(solve(s1,s2,i,j+1),solve(s1,s2,i+1,j));//return the max value of every option in the recursion untill the base condition is met 
+        if(dp[i][j]!=null)return dp[i][j];
+        // take aur skip ka hume logic lagana hain 
+        if(text1.charAt(i)==text2.charAt(j)){
+            return 1+solve(text1,text2,i+1,j+1);
+        }
+        return dp[i][j]= Math.max(solve(text1,text2,i+1,j),solve(text1,text2,i,j+1));
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        // method 1 -->rec+memo
+        dp=new Integer[text1.length()+1][text2.length()+1];
+        return solve(text1,text2,0,0);
+        
     }
 }
